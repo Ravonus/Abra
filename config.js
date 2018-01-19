8ec0b374334ec6d21c9b4ebdfe08e813ebf45231
Ravonus <chadkoslovsky@gmail.com>
2018-01-18 17:59:32 -0700

    Seperated config into its own local module. Begin step to seperating listfiles function.

---
+++ config.js
@@ -0,0 +1,86 @@
+
+const fs = require('fs');
+let phaserPath, port, x, firstValue, superKey;
+let newMath = 0;
+let filelist = [];
+let configArr = [];
+let math = new Object();
+dirPath = __dirname;
+let configVariables = new Object();
+let phaserConfig = new Object();
+configVariables.variables = new Object();
+
+// Look at the config file within main Abra directory. You can put values you don't want people to see here.
+//Keep in mind if its a public value - Abra still has to push it to the DIV so phaser has access. Abra will not push anything within abraConfig to phaser. This is default abra configs.
+let configString = fs.readFileSync(`${dirPath}/config.json`);
+let configJSON = JSON.parse(configString);
+
+
+
+
+if (configJSON.abraConfig) {
+  console.log("THIS SHIT BETTER BE RUNNING")
+  //DO stuff with abra configs
+  port = process.env.PORT || configJSON.abraConfig.phaserPort;
+  phaserPath = 'assets/' || configJSON.abraConfig.phaserPath;
+
+  //now delete object from configJSON. We don't want phaser to see theses.
+  delete configJSON['abraConfig'];
+}
+//check for custom variables.
+if (configJSON.variables) {
+
+  //   configVariables.variables = {}
+  for (i = 0; i < Object.keys(configJSON.variables).length; i++) {
+    let key = Object.keys(configJSON.variables)[i];
+    configVariables.variables[key] = configJSON.variables[key];
+
+  }
+  delete configJSON['variables'];
+  configVariablesArr = Array.from(configVariables.variables);
+  configArr = configArray(configVariables.variables);
+}
+if (configJSON.spritesheet) {
+  if (!phaserConfig.spritesheet) {
+    phaserConfig['spritesheet'] = new Object(configJSON.spritesheet);
+    //now delete object from configJSON. We don't want it to write into other.
+    delete configJSON['spritesheet'];
+
+  } else {
+    for (key in Object.keys(configJSON.spritesheet)) {
+      objectName = Object.keys(configJSON.spritesheet)[key];
+      phaserConfig.spritesheet[objectName] = new Object(configJSON.spritesheet[objectName]);
+      //now delete object from configJSON. We don't want it to write into other.
+
+    }
+    delete configJSON['spritesheet'];
+  }
+
+}
+
+let objName = 'abraMain'
+if (!phaserConfig[objName]) {
+  phaserConfig[objName] = new Object();
+}
+for (i = 0; i < Object.keys(configJSON).length; i++) {
+  let first = Object.keys(configJSON)[i];
+  let newObj = configJSON[first];
+  if (!phaserConfig[objName][first]) {
+    phaserConfig[objName][first] = new Object();
+  }
+  let second = Object.keys(newObj)[i]
+  phaserConfig[objName][first] = newObj;
+}
+
+
+
+
+
+
+
+  exports.PhaserConfig = phaserConfig;
+  exports.PhaserPath = phaserPath;
+  exports.Port = port;
+  exports.ConfigJSON = configJSON;
+  exports.Filelist = filelist;
+  exports.ConfigVariables = configVariables;
--- lister.js
+++ lister.js
@@ -1,33 +1,20 @@
-const express = require('express');
-const hbs = require('hbs');
-const fs = require('fs');
-let Parser = require('expr-eval').Parser;
-const app = express();
-const replacer = require('./replacer.js')
-const config = require('./config')
-const dirPath = __dirname
-
-let lastFile = [];
-
-
-//function to replace variable keys with actual value. (This also does math if you set the correct flag within your variable values)
-
 // function to get all files inside of directory and each directory inside of this. Used for HBS to push to front end. Automatic asset add into phaser.
+const fs = require('fs');
 let firstPhaserPath, blacklisted;
 let phaserNewPath;
 let dirPhaserArr = [];
 
-const listAllFiles = function (dir, filelist) {
+const listFiles = function (dir, filelist) {
 
   files = fs.readdirSync(dir);
   filelist = filelist || [];
 
   files.forEach(function (file) {
     if (fs.statSync(dir + '/' + file).isDirectory()) {
-      filelist = listAllFiles(dir + file + '/', filelist);
+      filelist = listFiles(dir + file + '/', filelist);
     }
     else {
-      config.ConfigJSON.blacklist.forEach((blacklist) => {
+      configJSON.blacklist.forEach((blacklist) => {
         if (file === blacklist) {
           blacklisted = true;
         }
@@ -71,25 +58,25 @@ const listAllFiles = function (dir, filelist) {
             //   configVariables.variables = {}
             for (i = 0; i < Object.keys(configJSON.variables).length; i++) {
               let key = Object.keys(configJSON.variables)[i];
-              config.ConfigVariables.variables[key] = configJSON.variables[key];
+              configVariables.variables[key] = configJSON.variables[key];
 
             }
             delete configJSON['variables'];
-            configVariablesArr = Array.from(config.ConfigVariables.variables);
-            configArr = configArray(config.ConfigVariables.variables);
+            configVariablesArr = Array.from(configVariables.variables);
+            configArr = configArray(configVariables.variables);
           }
           let objName = dir.match(/([^\/]*)\/*$/)[1];
-          if (!config.PhaserConfig[objName]) {
-            config.PhaserConfig[objName] = new Object();
+          if (!phaserConfig[objName]) {
+            phaserConfig[objName] = new Object();
           }
           for (i = 0; i < Object.keys(configJSON).length; i++) {
             let first = Object.keys(configJSON)[i];
             let newObj = configJSON[first];
-            if (!config.PhaserConfig[objName][first]) {
-              config.PhaserConfig[objName][first] = new Object();
+            if (!phaserConfig[objName][first]) {
+              phaserConfig[objName][first] = new Object();
             }
             let second = Object.keys(newObj)[i]
-            config.PhaserConfig[objName][first] = newObj;
+            phaserConfig[objName][first] = newObj;
           }
 
           if (configJSON.spritesheet) {
@@ -114,12 +101,12 @@ const listAllFiles = function (dir, filelist) {
           let configString = fs.readFileSync(dir + file);
           let configJSON = JSON.parse(configString);
 
-          if (!config.PhaserConfig['spritesheet']) {
-            config.PhaserConfig['spritesheet'] = new Object(configJSON);
+          if (!phaserConfig['spritesheet']) {
+            phaserConfig['spritesheet'] = new Object(configJSON);
           } else {
             for (obj in Object.keys(configJSON)) {
               key = Object.keys(configJSON)[obj];
-              config.PhaserConfig['spritesheet'][key] = configJSON[key]
+              phaserConfig['spritesheet'][key] = configJSON[key]
             }
           }
           return;
@@ -144,7 +131,7 @@ const listAllFiles = function (dir, filelist) {
 
         dirArr.forEach((directory) => {
           if (!firstPhaserPath) {
-            if (config.PhaserPath.includes(directory)) {
+            if (phaserPath.includes(directory)) {
               firstPhaserPath = true;
               phaserNewPath = `${directory}/`
             }
@@ -181,99 +168,6 @@ const listAllFiles = function (dir, filelist) {
   return filelist;
 };
 
-let configArray = ((obj) => {
-  let array = [];
-
-  for (i = 0; i < Object.keys(obj).length; i++) {
-    var key = Object.keys(obj)[i];
-    array.push(key, obj[key])
-
-  }
-  return array;
-
-})
-
-listAllFiles(`${dirPath}/public/${config.PhaserPath}`, config.Filelist)
-
-delete config.PhaserConfig['assets'].spritesheet;
-// loop through each custom variable and replace configs.
-// add math here?
-
-
-
-
-
-   // findAndReplace(phaserConfig, '${' + key + '}', variable, matchArr);
-  // console.log(configArr);
-
-   let objectTest = {
-    test1: {
-      erg: "1",
-      erg2: "${test}"
-    },
-    test2: {
-      erg: "${test}",
-      erg2: "2"
-    },
-    test3: {
-      erg: "${test}",
-      erg2: "2",
-      "erg3": "123132 123213 ${test} ${math} ${test2}  ${test} toob ${math} chamone ${math}",
-      "erg4": "123132 123213 ${test} ${math} ${test2}  ${test} toob ${math} chamone ${math} ${math2}",
-      "erg5": "${math2} ${math2} ${math2} ${math2} ${math2} ${math2}",
-      "math": "${math}"
-    }
-  }
-  
-  var varObj = {
-    '\\${test}': "FUCK",
-    '\\${test2}': "fuck2",
-    '\\${math}': "10+1+1000_4/3_3",
-    '\\${math2}': "1*0_2+1"
-  
-  };
-
- //  console.log(replacer.replaceAll(objectTest, varObj));
-
-    x = 0
-
-
-const filesObj = {
-  files: {
-    fileName: [],
-    filePath: []
-  }
+module.exports = {
+  ListFiles: listFiles
 };
-
-//console.log(configVariables.variables);
-
-//phaserConfig.assets = replacer.ReplaceAll(phaserConfig.assets, configVariables.variables);
-//console.log(phaserConfig.assets.spritesheet2)
-config.PhaserConfig = replacer.ReplaceAll(config.PhaserConfig, config.ConfigVariables.variables);
-config.PhaserConfig = JSON.parse(config.PhaserConfig);
-console.log(config.PhaserConfig);
-app.set('view engine', 'hbs');
-
-hbs.registerPartials(__dirname + '/views/partials');
-app.use(express.static(__dirname + '/public'));
-
-app.use((req, res, next) => {
-  let now = new Date().toString();
-  let log = `${now}: ${req.method} ${req.url} ${req.ip}`;
-
-  fs.appendFile(__dirname + '/public/server.log', log + '\n', (err) => {
-    if (err) {
-      console.log('Unable to append to server.log.');
-    }
-  });
-  next();
-});
-
-app.get('/', (req, res) => {
-  res.render('index.hbs', { img: JSON.stringify(config.Filelist), config: JSON.stringify(config.PhaserConfig) });
-
-});
-
-app.listen(config.Port, () => {
-  console.log(`Server started on port:${config.Port}.`);
-});
